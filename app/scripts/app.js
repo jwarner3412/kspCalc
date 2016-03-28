527bacd3213753bca4eabdbfbad974db8c3c9b3a
james <jwarner3412@gmail.com>
2016-03-28 16:38:04 -0400

    got period form wired

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,10 +5,28 @@ var numOfSatsInput = document.getElementById('numOfSats');
 var targetSelector = document.getElementById('targetType');
 
 var targetAlt = document.getElementById('targetAlt');
+var altForm = targetAlt.querySelector('label input');
+
 var targetPer = document.getElementById('targetPer');
+var secForm = targetPer.querySelector('label input[name="seconds"]');
+var minForm = targetPer.querySelector('label input[name="minutes"]');
+var hourForm = targetPer.querySelector('label input[name="hours"]');
+var dayForm = targetPer.querySelector('label input[name="days"]');
+
 var submit = document.getElementById('submit');
 
 var userInput;
+
+// sets form to 0 when alternate form submitted
+function zeroPerForm() {
+  secForm.value = 0;
+  minForm.value = 0;
+  hourForm.value = 0;
+  dayForm.value = 0;
+}
+function zeroAltForm() {
+  altForm.value = 0;
+}
 
 function importData(url) {
   // populate #bodyName select with json data
@@ -37,6 +55,40 @@ function importData(url) {
   xhr.send();
 }
 
+// gather info on submit
+function submitForm () {
+  // prepare body object
+  bodyObjectList.forEach(function(body) {
+    if (body.bodyName === userInput.body) {
+      userInput.body = body;
+    }
+  });
+
+  // gather form info, set unset values to 0
+  if (userInput.targBy === 'Altitude') {
+    zeroPerForm();
+    userInput.targAlt = parseInt(altForm.value, 10);
+    if (altForm.value < userInput.body.minPE) {
+      alert('Please enter a target altitude higher than ' + userInput.body.minPE + '.');
+    }
+  } else if (userInput.targBy === 'Period') {
+    zeroAltForm();
+    var periodList = targetPer.querySelectorAll('label input');
+    // check to make sure all entries are numbers, if not set to 0
+    for (var i = 0; i<periodList.length;i++) {
+      if (periodList[i].value === '') {
+        periodList[i].value = 0;
+      }
+    }
+  }
+  // do time maths to return info to userInput obj in seconds
+  var timeSec = parseInt(secForm.value, 10);
+  timeSec += parseInt(minForm.value, 10) *60;
+  timeSec += parseInt(hourForm.value, 10) *3600;
+  timeSec += parseInt(dayForm.value, 10) * userInput.body.siderealDayS;
+  userInput.targPer = timeSec;
+}
+
 // bind and define event handlers
 function eventTriggers() {
   function targSel(targ) {
@@ -59,12 +111,10 @@ function eventTriggers() {
   }
   // calls before input to set default
   targSel(targetSelector);
-
   // bind onchange event handler to target select
   targetSelector.addEventListener('change', function() {
     targSel(this);
   });
-
   // bind submit button
   submit.addEventListener('click', function(evt) {
     // on submit, collect form data and prepare proper body objects for maths
@@ -74,30 +124,7 @@ function eventTriggers() {
       satNum: numOfSatsInput.value,
       targBy: targetSelector.value
     };
-
-    // prepare body object
-    bodyObjectList.forEach(function(body) {
-      if (body.bodyName === userInput.body) {
-        userInput.body = body;
-      }
-    });
-
-    if (userInput.targBy === 'Altitude') {
-      userInput.targAlt = parseInt(targetAlt.querySelector('label input').value, 10);
-    } else if (userInput.targBy === 'Period') {
-      targetPer.querySelector('label input').forEach(function(data){
-        if (data.value === NaN) {
-          data.value = 0;
-        }
-      });
-      var timeSec = parseInt(targetPer.querySelector('label input[name="seconds"]').value, 10);
-      timeSec += parseInt(targetPer.querySelector('label input[name="minutes"]').value, 10) *60;
-      timeSec += parseInt(targetPer.querySelector('label input[name="hours"]').value, 10) *3600;
-      timeSec += parseInt(targetPer.querySelector('label input[name="days"]').value, 10) * userInput.body.siderealDayS;
-      userInput.targPer = timeSec;
-      console.log(userInput.targPer);
-      console.log(timeSec);
-    }
+    submitForm();
   });
 }
 
