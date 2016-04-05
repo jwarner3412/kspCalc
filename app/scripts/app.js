0795b07b0f7bd46f4796a46b9d0dc781f32f6f25
James Warner <jwarner3412@gmail.com>
2016-04-05 07:49:42 -0400

    still fixing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,14 @@
 var dataList;
-var userInput = {};
-var inputSelectors = {
+var userInput = {
+  recordInitInfo: function() {
+    this.targBySelect = $form.targBySelect.value;
+    this.precInput = parseFloat($form.precInput.value);
+    this.satCount = parseInt($form.satCount.value, 10);
+    this.body = dataList[$form.solSelect.value].bodys[$form.bodySelect.value];
+    return this;
+  }
+};
+var $form = {
   instructionButton: document.getElementById('instructionButton'),
   doMaths: document.getElementById('doMaths'),
   solSelect: document.getElementById('solSelect'),
@@ -13,7 +21,7 @@ var inputSelectors = {
   perHourInput: document.getElementById('perHourInput'),
   perMinInput: document.getElementById('perMinInput'),
   perSecInput: document.getElementById('perSecInput'),
-  bodySelectList: []
+  optList: []
 };
 var outputSelectors = {};
 
@@ -23,35 +31,119 @@ function importData(url, callback) {
   xhr.onreadystatechange = function() {
     if (xhr.readyState === XMLHttpRequest.DONE) {
       dataList = JSON.parse(xhr.responseText);
-      callback.call(inputSelectors, dataList, callback);
+      callback.call($form, dataList);
     } else {
-      console.log('Loading...');
+      console.log('Elevator music...');
     }
   };
   xhr.open('GET', url, true);
   xhr.send();
 }
-// inputSelectors methods
+// $form methods
 // creates select options from data provided by json loops
-inputSelectors.createOption = function(val, txt, addLoc) {
-  var option = document.createElement('option');
-  option.value = val;
-  option.text = txt;
-  addLoc.add(option);
-  console.log(val + txt);
-  return this;
-};
-inputSelectors.solOptionBuild = function(data, callback) {
+$form.selectBuild = function(data) {
+  function crtOpt(val, txt, addLoc) {
+    var option = document.createElement('option');
+    option.value = val;
+    option.text = txt;
+    if (typeof addLoc.add === 'function') {
+      addLoc.add(option);
+    } else if (typeof addLoc.push === 'function') {
+      addLoc.push(option);
+    } else {
+      console.log('fail');
+    }
+  }
   data.forEach(function(obj, index) {
-    inputSelectors.createOption(index, obj.name, this.solSelect);
+    crtOpt(index, obj.name, this.solSelect);
+    var bodyList = [];
+    data[index].bodys.forEach(function(array, ii) {
+      crtOpt(ii, array.bodyName, bodyList);
+    });
+    $form.optList.push(bodyList);
   });
-  callback.call(inputSelectors, data);
-};
-inputSelectors.bodyOptionBuild = function(data) {
-  data[this.solSelect.value].bodys.forEach(function(array, ii) {
-    inputSelectors.createOption(ii, array.bodyName, this.bodySelect);
-  });
-  return this;
+  $form.selectHandler(eventTriggers);
 };
 
-importData('./scripts/data.json', inputSelectors.solOptionBuild(inputSelectors.bodyOptionBuild));
+$form.selectHandler = function(callback) {
+  if ($form.bodySelect.options.length > 0) {
+    $form.bodySelect.options.length = 0;
+  }
+  $form.optList[$form.solSelect.value].forEach(function(obj) {
+    this.bodySelect.add(obj);
+  });
+  if (typeof callback === 'function') {
+    callback();
+  }
+};
+
+function eventTriggers() {
+  targHandler();
+  function targHandler() {
+    function disableAlt() {
+      $form.targAltInput.disabled = false;
+      $form.perDayInput.disabled = true;
+      $form.perHourInput.disabled = true;
+      $form.perMinInput.disabled = true;
+      $form.perSecInput.disabled = true;
+    }
+    function disablePer() {
+      $form.targAltInput.disabled = true;
+      $form.perDayInput.disabled = false;
+      $form.perHourInput.disabled = false;
+      $form.perMinInput.disabled = false;
+      $form.perSecInput.disabled = false;
+    }
+    if ($form.targBySelect.value === 'alt') {
+      disablePer();
+    } else {
+      disableAlt();
+    }
+  }
+  function periodHandler() {
+    //userInput.perSecTot = Math.toSec();
+    userInput.recordInitInfo();
+  }
+/*
+  function altReset() {
+    $form.targAltInput.value = 0;
+    recordInitInfo(minAltMath, semiMajMathT, targPerMath, parseSec);
+  }
+  function bodyReset() {
+    $form.bodySelect.options.length = 0;
+    $form.targAltInput.value = 0;
+    bodyOptionBuild(recordInitInfo, minAltMath, semiMajMathT, targPerMath, parseSec);
+  }
+  */
+  $form.targBySelect.addEventListener('change', targHandler);
+  // sol selects
+  $form.solSelect.addEventListener('change', $form.selectHandler);
+  // body selects
+  $form.bodySelect.addEventListener('change', userInput.recordInitInfo.bind(userInput));
+  // alt input change
+  /*
+  $form.targAltInput.addEventListener('change', recordInitInfo(minAltMath, semiMajMathT, targPerMath, parseSec));
+  // satellite count change
+  $form.satCount.addEventListener('change', altReset);
+  // placement precsision change
+  $form.precInput.addEventListener('change', altReset);
+  // period input change
+  $form.perSecInput.addEventListener('change', periodHandler);
+  $form.perMinInput.addEventListener('change', periodHandler);
+  $form.perHourInput.addEventListener('change', periodHandler);
+  $form.perDayInput.addEventListener('change', periodHandler);
+  // targetby selects
+
+  // buttons
+    // instructionButton
+      // pull up full screen instructions
+    // doMaths
+      // isolate body object in userInput object
+      // all the math functions here
+      // results write in function call
+    // rtantenna button
+      // write in small table with antenna ranges
+*/
+}
+
+importData('./scripts/data.json', $form.selectBuild);
--- app/scripts/capture.js
+++ app/scripts/capture.js
@@ -1,9 +1,9 @@
 Math.toSec = function() {
   var daySec = parseFloat(userInput.body.siderealDayS);
-  var sec = parseFloat(inputSelectors.perSecInput.value);
-  var min = parseInt(inputSelectors.perMinInput.value, 10) * 60;
-  var hour = parseInt(inputSelectors.perHourInput.value, 10) * 3600;
-  var day = parseInt(inputSelectors.perDayInput.value, 10) * daySec;
+  var sec = parseFloat($form.perSecInput.value);
+  var min = parseInt($form.perMinInput.value, 10) * 60;
+  var hour = parseInt($form.perHourInput.value, 10) * 3600;
+  var day = parseInt($form.perDayInput.value, 10) * daySec;
   return (sec + min + hour + day);
 };
 Math.radians = function(degrees) {
@@ -12,15 +12,7 @@ Math.radians = function(degrees) {
 
 // userInput methods
 // take a guess
-userInput.prototype.recordInitInfo = function() {
-  this.solSelect = inputSelectors.solSelect.value;
-  this.bodySelect = inputSelectors.bodySelect.value;
-  this.targBySelect = inputSelectors.targBySelect.value;
-  this.precInput = parseFloat(inputSelectors.precInput.value);
-  this.satCount = parseInt(inputSelectors.satCount.value, 10);
-  this.body = dataList[this.solSelect].bodys[this.bodySelect];
-  return this;
-}
+
 userInput.prototype.minAltMath = function() {
   var bodyR = userInput.body.radiusM;
   var minPE = userInput.body.minPE;
@@ -28,11 +20,11 @@ userInput.prototype.minAltMath = function() {
   var precInput = userInput.precInput;
   var rads = Math.radians(180 / satCount + precInput);
   var minAlt = Math.max(Math.ceil(bodyR / Math.cos(rads) - bodyR), minPE);
-  var tAltVal = parseFloat(inputSelectors.targAltInput.value);
+  var tAltVal = parseFloat($form.targAltInput.value);
   if (tAltVal === '' || tAltVal < minAlt) {
-    inputSelectors.targAltInput.value = minAlt;
+    $form.targAltInput.value = minAlt;
   }
-  userInput.targAltInput = parseFloat(inputSelectors.targAltInput.value);
+  userInput.targAltInput = parseFloat($form.targAltInput.value);
   return this;
 }
 userInput.prototype.semiMajMathT = function() {
@@ -48,100 +40,37 @@ userInput.prototype.targPerMath = function() {
 userInput.prototype.parseSec = function() {
   if (userInput.perSecTot > 60) {
     var secondsR = userInput.perSecTot % 60;
-    inputSelectors.perSecInput.value = secondsR;
+    $form.perSecInput.value = secondsR;
     var minutes = (userInput.perSecTot - secondsR) / 60;
     if (minutes > 60) {
       var minutesR = minutes % 60;
-      inputSelectors.perMinInput.value = minutesR;
+      $form.perMinInput.value = minutesR;
       var hours = (minutes - minutesR) / 60;
       if (hours > (userInput.body.siderealDayS / 3600)) {
         var hoursR = hours % (userInput.body.siderealDayS / 3600);
-        inputSelectors.perHourInput.value = hoursR;
+        $form.perHourInput.value = hoursR;
         var days = (hours - hoursR) / (userInput.body.siderealDayS / 3600);
         if (days > 0) {
-          inputSelectors.perDayInput.value = days;
+          $form.perDayInput.value = days;
         } else {
-          inputSelectors.perDayInput.value = 0;
+          $form.perDayInput.value = 0;
         }
       } else {
-        inputSelectors.perHourInput.value = hours;
-        inputSelectors.perDayInput.value = 0;
+        $form.perHourInput.value = hours;
+        $form.perDayInput.value = 0;
       }
     } else {
-      inputSelectors.perMinInput.value = minutes;
-      inputSelectors.perHourInput.value = 0;
-      inputSelectors.perDayInput.value = 0;
+      $form.perMinInput.value = minutes;
+      $form.perHourInput.value = 0;
+      $form.perDayInput.value = 0;
     }
   } else {
-    inputSelectors.perSecInput.value = userInput.perSecTot;
-    inputSelectors.perMinInput.value = 0;
-    inputSelectors.perHourInput.value = 0;
-    inputSelectors.perDayInput.value = 0;
+    $form.perSecInput.value = userInput.perSecTot;
+    $form.perMinInput.value = 0;
+    $form.perHourInput.value = 0;
+    $form.perDayInput.value = 0;
   }
   return this;
 }
 
 // bind event triggers - sol, body, targetby selects, bind buttons
-function eventTriggers() {
-  targHandler();
-  function targHandler() {
-    function disableAlt() {
-      inputSelectors.targAltInput.disabled = false;
-      inputSelectors.perDayInput.disabled = true;
-      inputSelectors.perHourInput.disabled = true;
-      inputSelectors.perMinInput.disabled = true;
-      inputSelectors.perSecInput.disabled = true;
-    }
-    function disablePer() {
-      inputSelectors.targAltInput.disabled = true;
-      inputSelectors.perDayInput.disabled = false;
-      inputSelectors.perHourInput.disabled = false;
-      inputSelectors.perMinInput.disabled = false;
-      inputSelectors.perSecInput.disabled = false;
-    }
-    if (inputSelectors.targBySelect.value === 'alt') {
-      disablePer();
-    } else {
-      disableAlt();
-    }
-  }
-  function periodHandler() {
-    userInput.perSecTot = Math.toSec();
-    recordInitInfo(minAltMath, semiMajMathT, targPerMath, parseSec);
-  }
-  function altReset() {
-    inputSelectors.targAltInput.value = 0;
-    recordInitInfo(minAltMath, semiMajMathT, targPerMath, parseSec);
-  }
-  function bodyReset() {
-    inputSelectors.bodySelect.options.length = 0;
-    inputSelectors.targAltInput.value = 0;
-    bodyOptionBuild(recordInitInfo, minAltMath, semiMajMathT, targPerMath, parseSec);
-  }
-  // sol selects
-  inputSelectors.solSelect.addEventListener('change', bodyReset);
-  // body selects
-  inputSelectors.bodySelect.addEventListener('change', altReset);
-  // alt input change
-  inputSelectors.targAltInput.addEventListener('change', recordInitInfo(minAltMath, semiMajMathT, targPerMath, parseSec));
-  // satellite count change
-  inputSelectors.satCount.addEventListener('change', altReset);
-  // placement precsision change
-  inputSelectors.precInput.addEventListener('change', altReset);
-  // period input change
-  inputSelectors.perSecInput.addEventListener('change', periodHandler);
-  inputSelectors.perMinInput.addEventListener('change', periodHandler);
-  inputSelectors.perHourInput.addEventListener('change', periodHandler);
-  inputSelectors.perDayInput.addEventListener('change', periodHandler);
-  // targetby selects
-  inputSelectors.targBySelect.addEventListener('change', targHandler);
-  // buttons
-    // instructionButton
-      // pull up full screen instructions
-    // doMaths
-      // isolate body object in userInput object
-      // all the math functions here
-      // results write in function call
-    // rtantenna button
-      // write in small table with antenna ranges
-}
