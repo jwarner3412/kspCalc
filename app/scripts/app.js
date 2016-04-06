a88df588b6fabc235b980608776f350ee2706424
James Warner <jwarner3412@gmail.com>
2016-04-06 07:47:56 -0400

    more stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,4 @@
 var dataList;
-var userInput = {
-  recordInitInfo: function() {
-    this.targBySelect = $form.targBySelect.value;
-    this.precInput = parseFloat($form.precInput.value);
-    this.satCount = parseInt($form.satCount.value, 10);
-    this.body = dataList[$form.solSelect.value].bodys[$form.bodySelect.value];
-    return this;
-  }
-};
 var $form = {
   instructionButton: document.getElementById('instructionButton'),
   doMaths: document.getElementById('doMaths'),
@@ -21,6 +12,7 @@ var $form = {
   perHourInput: document.getElementById('perHourInput'),
   perMinInput: document.getElementById('perMinInput'),
   perSecInput: document.getElementById('perSecInput'),
+  perInput: document.getElementsByClassName('perInputs'),
   optList: []
 };
 var outputSelectors = {};
@@ -31,7 +23,8 @@ function importData(url, callback) {
   xhr.onreadystatechange = function() {
     if (xhr.readyState === XMLHttpRequest.DONE) {
       dataList = JSON.parse(xhr.responseText);
-      callback.call($form, dataList);
+      console.log('data parsed');
+      callback.call($form);
     } else {
       console.log('Elevator music...');
     }
@@ -41,7 +34,7 @@ function importData(url, callback) {
 }
 // $form methods
 // creates select options from data provided by json loops
-$form.selectBuild = function(data) {
+$form.selectBuild = function(callback) {
   function crtOpt(val, txt, addLoc) {
     var option = document.createElement('option');
     option.value = val;
@@ -54,56 +47,80 @@ $form.selectBuild = function(data) {
       console.log('fail');
     }
   }
-  data.forEach(function(obj, index) {
+
+  dataList.forEach(function(obj, index) {
     crtOpt(index, obj.name, this.solSelect);
     var bodyList = [];
-    data[index].bodys.forEach(function(array, ii) {
+    dataList[index].bodys.forEach(function(array, ii) {
       crtOpt(ii, array.bodyName, bodyList);
     });
     $form.optList.push(bodyList);
   });
-  $form.selectHandler(eventTriggers);
-};
-
-$form.selectHandler = function(callback) {
-  if ($form.bodySelect.options.length > 0) {
-    $form.bodySelect.options.length = 0;
-  }
-  $form.optList[$form.solSelect.value].forEach(function(obj) {
-    this.bodySelect.add(obj);
-  });
+  console.log('selects build from json data');
   if (typeof callback === 'function') {
     callback();
   }
+  return this;
 };
 
-function eventTriggers() {
-  targHandler();
-  function targHandler() {
-    function disableAlt() {
-      $form.targAltInput.disabled = false;
-      $form.perDayInput.disabled = true;
-      $form.perHourInput.disabled = true;
-      $form.perMinInput.disabled = true;
-      $form.perSecInput.disabled = true;
-    }
-    function disablePer() {
-      $form.targAltInput.disabled = true;
-      $form.perDayInput.disabled = false;
-      $form.perHourInput.disabled = false;
-      $form.perMinInput.disabled = false;
-      $form.perSecInput.disabled = false;
-    }
-    if ($form.targBySelect.value === 'alt') {
-      disablePer();
-    } else {
-      disableAlt();
-    }
+$form.selectHandler = function(callback) {
+  if (this.bodySelect.options.length > 0) {
+    this.bodySelect.options.length = 0;
   }
-  function periodHandler() {
-    //userInput.perSecTot = Math.toSec();
-    userInput.recordInitInfo();
+  $form.optList[this.solSelect.value].forEach(function(obj) {
+    this.bodySelect.add(obj);
+  });
+  console.log('inserting body selects')
+  if (typeof callback === 'function') {
+    callback();
   }
+  return this;
+};
+$form.targHandler = function(callback) {
+  var bool;
+  if (this.targBySelect.value === 'alt') {
+    bool = false;
+  } else {
+    bool = true;
+  }
+  this.targAltInput.disabled = bool;
+  for (var i = 0; i < $form.perInput.length; i++) {
+
+    $form.perInput[i].disabled = !bool;
+  }
+  console.log('handling targetBy inputs')
+  if (typeof callback === 'function') {
+    callback();
+  }
+}
+$form.eventTriggers = function() {
+  $form.targHandler();
+
+  $form.targBySelect.addEventListener('change', $form.targHandler.bind(this));
+  // sol selects
+  $form.solSelect.addEventListener('change', $form.selectHandler.bind(this, this.recordBody));
+  // body selects
+  $form.bodySelect.addEventListener('change', $form.recordBody.bind(this));
+  // alt input change
+  /*
+  $form.targAltInput.addEventListener('change', recordInitInfo(minAltMath, semiMajMathT, targPerMath, parseSec));
+  // satellite count change
+  $form.satCount.addEventListener('change', altReset);
+  // placement precsision change
+  $form.precInput.addEventListener('change', altReset);
+*/
+  console.log('triggers set')
+}
+$form.recordBody = function(callback) {
+  this.userBody = dataList[this.solSelect.value].bodys[this.bodySelect.value];
+  console.log('recording body');
+  if (typeof callback === 'function') {
+    callback();
+  }
+  return this;
+}
+
+importData('./scripts/data.json', $form.selectBuild.bind($form, $form.selectHandler));
 /*
   function altReset() {
     $form.targAltInput.value = 0;
@@ -115,35 +132,3 @@ function eventTriggers() {
     bodyOptionBuild(recordInitInfo, minAltMath, semiMajMathT, targPerMath, parseSec);
   }
   */
-  $form.targBySelect.addEventListener('change', targHandler);
-  // sol selects
-  $form.solSelect.addEventListener('change', $form.selectHandler);
-  // body selects
-  $form.bodySelect.addEventListener('change', userInput.recordInitInfo.bind(userInput));
-  // alt input change
-  /*
-  $form.targAltInput.addEventListener('change', recordInitInfo(minAltMath, semiMajMathT, targPerMath, parseSec));
-  // satellite count change
-  $form.satCount.addEventListener('change', altReset);
-  // placement precsision change
-  $form.precInput.addEventListener('change', altReset);
-  // period input change
-  $form.perSecInput.addEventListener('change', periodHandler);
-  $form.perMinInput.addEventListener('change', periodHandler);
-  $form.perHourInput.addEventListener('change', periodHandler);
-  $form.perDayInput.addEventListener('change', periodHandler);
-  // targetby selects
-
-  // buttons
-    // instructionButton
-      // pull up full screen instructions
-    // doMaths
-      // isolate body object in userInput object
-      // all the math functions here
-      // results write in function call
-    // rtantenna button
-      // write in small table with antenna ranges
-*/
-}
-
-importData('./scripts/data.json', $form.selectBuild);
