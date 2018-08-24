d09fde5ddabe209d5055ff4006560b674da8ee43
James Warner <jwarner3412@gmail.com>
2018-08-24 15:58:49 -0400

    Initial commit of rewrite. Will be stripping all unneeded tools out of the project and assembling my own gulp file. Re-writing app with es6 syntax, and hopefully less clunky now that i have a better grasp on some things.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,6 @@
-// imports json datafile of solar systems and thier body objects and properties.
-// builds select options from the data, stores them in a 2d array.
-// sets initial values, disables proper form inputs, and binds triggers.
-// have alot of if(callback) blocks, wasnt sure if i was going to call back or
-// method chain. method chain mostly won out for sync stuff.
-(function() {
-  $form.importData('./data.json', function(b) {
-    $form.selectBuild(b).selectHandler().targHandler().eventTriggers();
-  });
-})();
-
-// antenna info for the results page
-// ends at import, dont have handler yet
-(function() {
-  antList = $form.importData('./ant.json');
-})();
-
-// (c) Copyright 2015 person or company, all rights reserved.
+// 1. add table and input fields
+// 2. bind to inputs
+// 3. ajax call for json datafile
+// 4. set default values
+//     a. do math for rest / use stored min/max values?, fill in opposing fields, disable applicable inputs
+// 4. set event triggers
